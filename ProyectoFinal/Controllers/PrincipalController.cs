ed7eee4700d735994c41399ffa324300eaffd372
jcadavid52 <jccadavid17@misena.edu.co>
2022-03-07 18:35:06 -0500

    Etapa uno

---
+++ ProyectoFinal/Controllers/PrincipalController.cs
@@ -0,0 +1,116 @@
+﻿using ProyectoFinal.DatosCrud;
+using ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProyectoFinal.Controllers
+{
+    public class PrincipalController : Controller
+    {
+        LogicaNegocio logicaNegocio = new LogicaNegocio();
+       
+        // GET: Principal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Registro()
+        {
+            return View();
+        }
+        public ActionResult RegistroAccion(SESION nuevoRegistro)
+        {
+            
+            try
+            {
+                logicaNegocio.Registro(nuevoRegistro);
+                ViewBag.Mensaje = logicaNegocio.MensajeRegistro();
+            }catch(Exception e)
+            {
+                ViewBag.Mensaje = logicaNegocio.MensajeRegistro();
+            }
+            
+            return View("Index");
+        }
+
+        public ActionResult Acceso(SESION acceso)
+        {
+            SESION nuevo_acceso = logicaNegocio.Acceso(acceso);
+            if(nuevo_acceso != null)
+            {
+              Session["ID"] = nuevo_acceso.ID_SESION;
+                return View("IndexSesion");
+            
+            }
+            else
+            {
+            return View("Index");
+            }
+            
+        }
+        public ActionResult IndexSesion()
+        {
+           
+            return View();
+        }
+
+
+
+      
+        public ActionResult EspacioVirtual()
+        {
+            try
+            {
+            
+            IEnumerable<CATEGORIA> listarCategoria = logicaNegocio.ListarCategoria();
+             
+
+            return View(listarCategoria);
+         
+            }catch(Exception e)
+            {
+             return View(Session["ID"]);
+            }
+            
+            
+        }
+
+        public ActionResult EspacioVirtualAccion(PRODUCTO nuevoProducto)
+        {
+            try
+            {
+
+                logicaNegocio.RegistrarProducto(nuevoProducto);
+
+            }
+            catch (Exception e)
+            {
+                
+            }
+            return View();
+
+        }
+
+        public ActionResult PublicarUsuario(USUARIO usuario)
+        {
+            IEnumerable<CATEGORIA> listarCategoria = logicaNegocio.ListarCategoria();
+            try
+            {
+                logicaNegocio.PublicarUsuario(usuario);
+                
+                ViewBag.Mensaje = logicaNegocio.MensajeRegistro();
+            }
+            catch (Exception e)
+            {
+                ViewBag.Mensaje = logicaNegocio.MensajeRegistro();
+            }
+            return View("EspacioVirtual",listarCategoria);
+        }
+
+
+    }
+}
---
+++ ProyectoFinal/DatosCrud/LogicaNegocio.cs
@@ -0,0 +1,99 @@
+﻿using ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoFinal.DatosCrud
+{
+    public class LogicaNegocio
+    {
+        string mensaje;
+        
+        private string Mensaje { get => mensaje; set => mensaje = value; }
+        //Método que regitra al usuario
+        public void Registro(SESION registro)
+        {
+            using (BD_PROYECTO_FINALEntities1 bd_registro = new BD_PROYECTO_FINALEntities1())
+            {
+                try
+                {
+                bd_registro.REGISTRAR_USER(registro.USUARIO,registro.CLAVE);
+                bd_registro.SaveChanges();
+                    Mensaje = "Se registró con éxito, bienvenido al sistema";
+                }catch(Exception e)
+                {
+                    Mensaje = "Falla con el servidor, intente nuevamente";
+                }
+               
+            }
+            
+        }
+        //Mensaje que informa el éxito que tuvo el registro del usuario
+        public string MensajeRegistro()
+        {
+            return Mensaje;
+        }
+
+        public SESION Acceso(SESION VerificarSesion)
+        {
+            using(BD_PROYECTO_FINALEntities1 bd_VerificarSesion = new BD_PROYECTO_FINALEntities1())
+            {
+               return bd_VerificarSesion.SESION.FirstOrDefault(S => S.USUARIO == VerificarSesion.USUARIO && S.CLAVE == VerificarSesion.CLAVE);
+            }
+        }
+        //Método que lista las categorías
+        public IEnumerable<CATEGORIA> ListarCategoria()
+        {
+            using (BD_PROYECTO_FINALEntities1 bd_categoria = new BD_PROYECTO_FINALEntities1())
+            {
+               return bd_categoria.CATEGORIA.ToList();
+            }
+        }
+        //Método que registra el producto del usuario
+        public void RegistrarProducto(PRODUCTO nuevoProducto)
+        {
+            using (BD_PROYECTO_FINALEntities1 bd_nuevoProducto = new BD_PROYECTO_FINALEntities1())
+            {
+                try
+                {
+                    Mensaje = "Se registró el producto con éxito";
+                    bd_nuevoProducto.PRODUCTO.Add(nuevoProducto);
+                    bd_nuevoProducto.SaveChanges();
+                }
+                catch(Exception e)
+                {
+                    Mensaje = "Falla con el servidor, intente nuevamente" + e.Message;
+                }
+            
+            }
+        }
+
+        //Método que registra la publicación del usuario que ya ha iniciado sesión
+        public void PublicarUsuario(USUARIO usuario)
+        {
+            using (BD_PROYECTO_FINALEntities1 bd_usuario = new BD_PROYECTO_FINALEntities1())
+            {
+                try
+                {
+                    string nombre_usuario = usuario.NOMBRE_USUARIO;
+                    string apellido_usuario = usuario.APELLIDO_USUARIO;
+                    string telefono = usuario.TELEFONO;
+                    string celular = usuario.CELULAR;
+                    string correo = usuario.CORREO;
+                    string pais = usuario.PAIS;
+                    int? fk_sesion = usuario.FK_SESION;
+
+                    bd_usuario.USP_PUBLICAR_USER(nombre_usuario,apellido_usuario,telefono,celular,correo,pais,fk_sesion);
+                    bd_usuario.SaveChanges();
+                    Mensaje = "Te haz publicado exitosamente, ahora puedes mostrar tu talento";
+                }
+                catch (Exception e)
+                {
+                    Mensaje = "Falla con el servidor, intente nuevamente :(" + e.Message;
+                }
+            }
+        }
+
+    }
+}
